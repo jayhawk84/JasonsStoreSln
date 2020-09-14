0d96b7e8ce99f69d5db9509b442342bdff8c3301
Jason Horvath <47703915+jayhawk84@users.noreply.github.com>
2020-09-14 11:19:23 -0700

    This is my store of outdoor gear
    This is a class project that we work through using the book Pro ASP.NET Core 3.

---
+++ OutdoorProducts/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OutdoorProducts.Models;
+using System.Linq;
+
+namespace OutdoorProducts.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStoreRepository repository;
+        public int PageSize = 4;
+
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult Index(int productPage = 1) => View(repository.Products.OrderBy(p => p.ProductID).Skip((productPage - 1) * PageSize).Take(PageSize));
+    }
+}
